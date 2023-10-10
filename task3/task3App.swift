505b050baf0af79f35d8fc9499935e5f657002fe
Ehsan Abdolmajidi <ehsan.msc87@gmail.com>
2023-10-10 18:55:56 +0200

    Initial Commit

---
+++ task3/task3App.swift
@@ -0,0 +1,17 @@
+//
+//  task3App.swift
+//  task3
+//
+//  Created by Ehsan Abdolmajidi on 2023-10-10.
+//
+
+import SwiftUI
+
+@main
+struct task3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ task3UITests/task3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  task3UITestsLaunchTests.swift
+//  task3UITests
+//
+//  Created by Ehsan Abdolmajidi on 2023-10-10.
+//
+
+import XCTest
+
+final class task3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
