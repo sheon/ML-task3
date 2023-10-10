48d4ab18b93ed568f097c1e93dc04972e64614d3
Ehsan Abdolmajidi <ehsan.msc87@gmail.com>
2023-10-10 19:42:38 +0200

    Task3

---
+++ task3/FruitView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitView.swift
+//  task3
+//
+//  Created by Ehsan Abdolmajidi on 2023-10-10.
+//
+
+import SwiftUI
+
+struct FruitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitView()
+}
